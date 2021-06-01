9861fc66a44f73d9a55a84fe34c383457b8b23b9
gregory.feijon <gregory.feijon@outlook.com>
2021-05-31 21:01:36 -0300

    [UPDATE] Melhoria nas classes úteis.

--- src/main/java/br/com/gregoryfeijon/consultacep/security/config/ExceptionAdvice.java
+++ src/main/java/br/com/gregoryfeijon/consultacep/security/config/ExceptionAdvice.java
@@ -1,13 +1,20 @@
 package br.com.gregoryfeijon.consultacep.security.config;
 
 import java.nio.file.AccessDeniedException;
+import java.util.List;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.gregoryfeijon.consultacep.api.response.Response;
+import br.com.gregoryfeijon.consultacep.exception.APIException;
 import br.com.gregoryfeijon.consultacep.util.ApiUtil;
 
 /**
@@ -17,10 +24,24 @@ import br.com.gregoryfeijon.consultacep.util.ApiUtil;
  */
 
 @RestControllerAdvice
-public class ExceptionAdvice {
+public class ExceptionAdvice extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(value = { AccessDeniedException.class })
-	public <T> ResponseEntity<Response<T>> notAllowed() {
+	public final <T> ResponseEntity<Response<T>> notAllowed() {
 		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ApiUtil.criarResponseDeErro("Access not allowed"));
+	}
+
+	@ExceptionHandler(APIException.class)
+	public final <T> ResponseEntity<Response<T>> handleInternalException(APIException ex) {
+		return ResponseEntity.badRequest().body(ApiUtil.criarResponseDeErro(ex));
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		List<FieldError> errors = ex.getBindingResult().getFieldErrors();
+		StringBuilder sb = new StringBuilder("Erros de validação dos dados inseridos:\n");
+		errors.stream().forEach(erro -> sb.append(erro.getDefaultMessage()));
+		return ResponseEntity.badRequest().body(ApiUtil.criarResponseDeErro(sb.toString()));
 	}
 }
--- src/main/java/br/com/gregoryfeijon/consultacep/util/ApiUtil.java
+++ src/main/java/br/com/gregoryfeijon/consultacep/util/ApiUtil.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import br.com.gregoryfeijon.consultacep.api.response.Response;
+import br.com.gregoryfeijon.consultacep.exception.APIException;
 
 /**
  * 06/03/2021 às 21:12:54
@@ -16,6 +17,10 @@ public final class ApiUtil {
 	private static final LoggerUtil LOG = LoggerUtil.getLog(ApiUtil.class);
 
 	private ApiUtil() {}
+
+	public static <T> Response<T> criarResponseDeErro(APIException ex) {
+		return criarResponseDeErro(ex.getMessage());
+	}
 
 	public static <T> Response<T> criarResponseDeErro(String erro) {
 		return criarResponseDeErro(Arrays.asList(erro));
